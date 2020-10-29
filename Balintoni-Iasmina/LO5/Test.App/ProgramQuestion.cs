3c357a7762bcd2f8c5993058a2ef28ee33cb1b8c
animsai30 <iasmina.balintoni@gmail.com>
2020-10-29 08:16:36 +0200

    Tema 5

---
+++ Balintoni-Iasmina/LO5/Test.App/ProgramQuestion.cs
@@ -0,0 +1,54 @@
+﻿using LanguageExt;
+using Question.Domain.PostQuestionWorkflow;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using static Question.Domain.PostQuestionWorkflow.PostQuestionResult;
+using static Question.Domain.PostQuestionWorkflow.Question;
+
+namespace Test.App
+{
+    class ProgramQuestion
+    {
+        static void Main(string[] args)
+        {
+            List<string> tags = new List<string> { "Python"};
+            
+             var result= UnverifiedQuestion.Create("What are tuples?How can you use them?", tags);
+
+            result.Match(
+                Succ: question =>
+                {
+                    StartQuestion(question);
+                    Console.WriteLine("Able to vote ");
+                    return Unit.Default;
+                },
+                Fail : ex =>
+                {
+                    Console.WriteLine($"Invalid question.Reason:{ex.Message}");
+                    return Unit.Default;
+                }
+                
+                );
+
+            Console.ReadLine();
+        }
+        private static void StartQuestion(UnverifiedQuestion question)
+        {
+            var verifiedQuestionResult = new VerifyQuestioService().VerifyQuestion(question);
+            verifiedQuestionResult.Match(
+                verifiedQuestion =>
+                {
+                    new VoteService().SendVote(verifiedQuestion);
+                    return Unit.Default;
+                },
+                ex =>
+                {
+                    Console.WriteLine("Impossible to vote");
+                    return Unit.Default;
+                }
+                );
+        }
+        
+    }
+}
